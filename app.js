f09148632334a9a564ffceeb06fc7655dd68ec35
Yao <sukoyao1101@gmail>
2019-05-26 14:49:46 +0800

    Add seeder and view pages

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require('express')
 const app = express()
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const Url = require('./models/url')
 
-mongoose.connect('mongodb://localhost/url', { useNewUrlParser: true })   // 設定連線到 mongoDB
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.use(methodOverride('_method'))
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+mongoose.connect('mongodb://localhost/url', {
+  useNewUrlParser: true,
+  useCreateIndex: true
+})   // 設定連線到 mongoDB
 
 const db = mongoose.connection
 
@@ -16,12 +31,20 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-const Url = require('./models/url')
+
 
 app.get('/', (req, res) => {
-  res.send('hello world!')
+  res.render('index')
 })
 
-app.listen(3000, () => {
+app.post('/', (req, res) => {
+  res.send('寫入 urls')
+})
+
+app.get('/urls', (req, res) => {
+  res.render('new')
+})
+
+app.listen(process.env.PORT || 3000, () => {
   console.log('App is running!')
 })
---
+++ models/seeds/seeder.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+// const bcrypt = require('bcryptjs')
+const Url = require('../url')
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/url', {
+  useNewUrlParser: true,
+  useCreateIndex: true
+})
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('db error')
+})
+
+db.once('open', () => {
+  console.log('db connected!')
+
+  Url.create({
+    name: 'https://www.google.com/',
+    // key: bcrypt.hashSync(`https://www.google.com/`, 10).slice(-5)
+  })
+
+  console.log('done')
+})
